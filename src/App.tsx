3603b6e74bd63226ecc588eec75f2897dc0bb107
daniel johnson <junesveryown6ix@gmail.com>
2025-04-22 10:05:21 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="text-3xl font-bold text-blue-600">
+      URGENT 2KAY is live!
+    </div>
+    </>
+  )
+}
+
+export default App
